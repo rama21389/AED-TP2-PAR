e2caa39cd7061cfcd8b008e03100f0809c820697
rama21389 <ramiro.e.mendez@hotmail.com>
2015-04-20 17:18:44 -0300

    AED- TP2-PAR
    numero par o impar.

---
+++ AED - TP2-PAR.cpp
@@ -0,0 +1,25 @@
+/* Ramiro Ezequiel Mendez
+ * K1051
+ * AED - TP2- Par
+ */
+
+
+#include <iostream>
+
+int main ()
+{
+	int a;
+	
+	std::cout<< "Ingresar un numero:  \n";
+	std::cin>> a;
+	
+	if (a%2 ==0)
+	{ 
+	std::cout<< " El numero es par  \n";
+	}
+	else
+	{ 
+	std::cout<< "el numero es impar  \n"; 
+	}
+	return 0;
+};
